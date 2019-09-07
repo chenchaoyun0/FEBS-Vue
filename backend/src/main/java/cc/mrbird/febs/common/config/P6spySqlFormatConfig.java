bdfd4e4aa67851661e08c106fa1aab34361f0c02
chenchaoyun0 <873692191@qq.com>
2019-09-08 00:19:43 +0800

    Update P6spySqlFormatConfig.java

--- backend/src/main/java/cc/mrbird/febs/common/config/P6spySqlFormatConfig.java
+++ backend/src/main/java/cc/mrbird/febs/common/config/P6spySqlFormatConfig.java
@@ -19,6 +19,6 @@ public class P6spySqlFormatConfig implements MessageFormattingStrategy {
     @Override
     public String formatMessage(int connectionId, String now, long elapsed, String category, String prepared, String sql, String url) {
         return StringUtils.isNotBlank(sql) ? DateUtil.formatFullTime(LocalDateTime.now(), DateUtil.FULL_TIME_SPLIT_PATTERN)
-                + " | 耗时 " + elapsed + " ms | SQL 语句：" + StringUtils.LF + sql.replaceAll("[\\s]+", StringUtils.SPACE) + ";" : "";
+                + " | 耗时 " + elapsed + " ms | SQL 语句：" + StringUtils.LF + sql.replaceAll("[\\s]+", StringUtils.SPACE) + ";" : null;
     }
 }
